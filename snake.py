a514a8514b94e954b9fffa02c4529170a7e2432d
João Batista dos Santos Júnior <joaojuniorsantos2008@gmail.com>
2020-02-25 02:23:47 -0300

    Mapeamento das teclas e difinção do movimento da snake

--- snake.py
+++ snake.py
@@ -3,6 +3,12 @@
 import pygame
 from pygame.locals import *
 
+#Define a movimentação da cobra
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("Snake")
@@ -10,11 +16,37 @@ pygame.display.set_caption("Snake")
 snake = [(200, 200), (210, 200), (220,200)]
 snake_skin = pygame.Surface((10,10))
 snake_skin.fill((255,255,255)) #CorBranca
+
+direcao = LEFT
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
+            #Seta
+        if event.type == KEYDOWN:
+            if event.key == K_UP:
+                direcao = UP
+            if event.key == K_DOWN:
+                direcao = DOWN
+            if event.key == K_LEFT:
+                direcao = LEFT
+            if event.key == K_RIGHT:
+                direcao = RIGHT
+                
+    for i in range(len(snake) -1, 0, -1):
+        snake[i] = (snake[i-1][0], snake[i-1][1])
+        
+        if direcao == UP:
+            snake[0] = (snake[0][0], snake[0][1] - 10)
+        if direcao == DOWN:
+            snake[0] = (snake[0][0], snake[0][1] + 10)
+        if direcao == RIGHT:
+            snake[0] = (snake[0][0] + 10, snake[0][1])
+        if direcao == LEFT:
+            snake[0] = (snake[0][0] - 10, snake[0][1])
+            
     screen.fill((0,0,0))
     for pos in snake:
         screen.blit(snake_skin,pos)
