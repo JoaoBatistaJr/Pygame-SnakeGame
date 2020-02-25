909cb00cd17160bd250e25b608c0628ff2185513
João Batista dos Santos Júnior <joaojuniorsantos2008@gmail.com>
2020-02-25 01:01:59 -0300

    Tela de loop padrão do jogo.

---
+++ snake.py
@@ -0,0 +1,14 @@
+# Cria janela vazia com loop do jogo #
+
+import pygame
+from pygame.locals import *
+
+pygame.init()
+screen = pygame.display.set_mode((600, 600))
+pygame.display.set_caption("Snake")
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+    pygame.display.update()
