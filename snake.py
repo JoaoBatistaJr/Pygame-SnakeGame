04915408a6ac3b1d206a85d94e7230eeec1d80a4
João Batista dos Santos Júnior <joaojuniorsantos2008@gmail.com>
2020-02-25 01:13:40 -0300

    Adicionei a snake na tela

--- snake.py
+++ snake.py
@@ -6,9 +6,16 @@ from pygame.locals import *
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("Snake")
+
+snake = [(200, 200), (210, 200), (220,200)]
+snake_skin = pygame.Surface((10,10))
+snake_skin.fill((255,255,255)) #CorBranca
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
+    screen.fill((0,0,0))
+    for pos in snake:
+        screen.blit(snake_skin,pos)
     pygame.display.update()
