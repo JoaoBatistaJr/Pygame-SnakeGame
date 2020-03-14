975c39ab68d3d790a9568b6971dcdf57cc81cc65
João Batista dos Santos Júnior <joaojuniorsantos2008@gmail.com>
2020-03-14 14:06:46 -0300

    Definição da snake e da apple

--- snake4.py
+++ snake4.py
@@ -16,6 +16,16 @@ pygame.init()
 screen = pygame.display.set_mode((540,480))
 pygame.display.set_caption('Snake Game 4.0')
 
+"""Define as Entidades do game"""
+#Defina a Snake
+snake = [(200, 200), (210, 200), (220, 200)]
+snake_skin = pygame.Surface((10, 10))
+snake_skin.fill(BRANCO)
+
+#Define a Maçã
+apple = pygame.Surface((10, 10))
+apple.fill(VERMELHO)
+
 #Game Loop
 while True:
     for event in pygame.event.get():
